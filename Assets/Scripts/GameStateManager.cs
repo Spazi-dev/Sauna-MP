97580140b83ee6638bb45b61e9b3c86659eb2329
Spazi <talishmarprkle+g@gmail.com>
2024-12-17 01:14:26 +0200

    testbuild

--- Assets/Scripts/GameStateManager.cs
+++ Assets/Scripts/GameStateManager.cs
@@ -9,6 +9,8 @@ public class GameStateManager : MonoBehaviour
 	public static GameStateManager Singleton { get; private set; }
 	[SerializeField] public GlobalRuleSet activeGlobalRules;
 	[SerializeField] GameObject StartMenuParent;
+	[SerializeField] GameObject SessionWidgetParent;
+	[SerializeField] GameObject NetworkedPointLight; // temporarily here because the proximity check test light disrupts the appearance of the character creation screen
 	public CosmeticItemCatalog CosmeticItemCatalog;
 	public CharacterSheet localCharacterSheet;
 	PlayerCharacterCreator playerCharacterCreator;
@@ -35,11 +37,15 @@ public class GameStateManager : MonoBehaviour
 		//print($"<color=#DD0088>Client starteddd </color>");
 		playerCharacterCreator.SaveCharacterSheetData();
 		StartMenuParent.SetActive(false);
+		//SessionWidgetParent.SetActive(true);
+		NetworkedPointLight.SetActive(true);
 	}
 	void QuitEvent(bool host)
 	{
 		//print($"<color=#FF00AA>Client quitted, hostmode: {host}</color>");
 		StartMenuParent.SetActive(true);
+		//SessionWidgetParent.SetActive(false);
+		NetworkedPointLight.SetActive(false);
 		playerCharacterCreator.Invoke("RemakeCharacter", 0);
 	}
 
